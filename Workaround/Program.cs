d2e2a4f43a053de114222bc718035399a32cd2fc
Huseyin <huseyin.kann@hotmail.com>
2024-02-11 13:23:19 +0300

    Bootcamp First Task

---
+++ Workaround/Program.cs
@@ -0,0 +1,18 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Person person = new Person();
+        person.FirstName = "Hüseyin";
+        person.LastName = "Kan";
+        person.BirthYear = 1999;
+        person.NationalId = 1234;
+
+        PttManager pttManager = new PttManager(new PersonManager());
+        pttManager.GiveMask(person);
+    }
+
+}
